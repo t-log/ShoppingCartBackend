39b6b2bec8023f81de1c7d3e22be0bfd35e1e515
akhil <akhil.paul@ymail.com>
2022-12-09 14:17:49 +0530

    Adds validation and local storage

--- src/main/java/com/example/shopping_backend/dao/ShoppingDao.java
+++ src/main/java/com/example/shopping_backend/dao/ShoppingDao.java
@@ -1,9 +1,15 @@
 package com.example.shopping_backend.dao;
 
 import com.example.shopping_backend.model.Shopping;
+import com.example.shopping_backend.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ShoppingDao extends CrudRepository<Shopping,Integer>
 {
-
+    @Query(value = "SELECT `id`, `category`, `description`, `name`, `price` FROM `products` WHERE `name`=:name",nativeQuery = true)
+    List<Shopping> searchProduct(@Param("name") String name);
 }
--- src/main/java/com/example/shopping_backend/dao/UserDao.java
+++ src/main/java/com/example/shopping_backend/dao/UserDao.java
@@ -7,6 +7,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserDao extends CrudRepository<User,Integer> {
-    @Query(value = "",nativeQuery = true)
-    List<User> searchUser(@Param("username") String title);
+    @Query(value ="SELECT `id`, `address`, `email`, `name`, `password`, `phone_no`, `username` FROM `users` WHERE `email`=:email AND `password`=:password",nativeQuery = true)
+    List<User> searchUser(@Param("email") String email,@Param("password") String password);
+
+    @Query(value ="SELECT * FROM `users` WHERE `id`=:id",nativeQuery = true)
+    List<User> getUser(@Param("id") Integer id);
 }
